e17cc006a08e7585b1b61c3ff2f93dc4bf32df2b
iam46479717 <arnaucr1997@gmail.com>
2019-10-10 17:49:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pt1cebolladaarnau/Principal.java
@@ -0,0 +1,25 @@
+package com.example.pt1cebolladaarnau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickHelp(View view){
+
+
+        Intent intent= new Intent(this, Ajuda.class);
+
+
+        startActivity(intent);
+
+    }
+}
